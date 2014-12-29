61a19895d14b9a0eecde02428e621e66e68b4756
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-12-29 23:48:12 +0200

    Test

--- build.sh
+++ build.sh
@@ -2,10 +2,15 @@
 
 echo Commit hash: ${ghprbActualCommit}
 echo Changes in this commit:
-git diff-tree --no-commit-id --name-only -r ${ghprbActualCommit}
+export COMMIT_CHANGES=`git diff-tree --no-commit-id --name-only -r ${ghprbActualCommit}`
+echo ${COMMIT_CHANGES}
 
-jsonlint -s -v `git diff-tree --no-commit-id --name-only -r ${ghprbActualCommit}`
+jsonlint -s -v ${COMMIT_CHANGES}
 
 # fetch latest netkan.exe
 wget -O netkan.exe http://ci.ksp-ckan.org:8080/job/NetKAN/lastSuccessfulBuild/artifact/netkan.exe
 
+for * in ${COMMIT_CHANGES}
+do
+	mono --debug netkan.exe ${COMMIT_CHANGES} --cachedir="." --outputdir="."
+done
