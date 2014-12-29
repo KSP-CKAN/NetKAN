18947635dc0c722b53e9ce1e33172ffa2a3aa30c
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-12-29 23:43:01 +0200

    Test

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 jsonlint -s -v NetKAN/*.netkan
 
 echo Commit hash: ${ghprbActualCommit}
+echo Changed files:
+git diff-tree --no-commit-id --name-only -r ${ghprbActualCommit}
 
 # export TRAVIS_PULL_REQUEST=true
 # prove
