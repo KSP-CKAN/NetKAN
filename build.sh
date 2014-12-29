126f81666dd5cfacf95314eb2ada5ecd4cbf1dd8
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-12-29 23:59:32 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
 
-jsonlint -s -v NetKAN/*.netkan
-
 echo Commit hash: ${ghprbActualCommit}
+echo Changes in this commit:
+export COMMIT_CHANGES=`git diff-tree --no-commit-id --name-only -r ${ghprbActualCommit}`
+echo ${COMMIT_CHANGES}
 
-# export TRAVIS_PULL_REQUEST=true
-# prove
+jsonlint -s -v ${COMMIT_CHANGES}
+
+# fetch latest netkan.exe
+wget -O netkan.exe http://ci.ksp-ckan.org:8080/job/NetKAN/lastSuccessfulBuild/artifact/netkan.exe
+
+mkdir built
+
+for f in ${COMMIT_CHANGES}
+do
+	mono --debug netkan.exe $f --cachedir="." --outputdir="built"
+done
+
+# fetch latest ckan.exe
+wget -O ckan.exe http://ci.ksp-ckan.org:8080/job/CKAN/lastSuccessfulBuild/artifact/ckan.exe
+
+# create a dummy KSP install
+mkdir dummy_ksp
+echo Version 0.90.0 > dummy_ksp/readme.txt
+mkdir dummy_ksp/GameData
+
+mono --debug ckan.exe ksp add default "`pwd`/dummy_ksp"
+mono --debug ckan.exe ksp default default
+
+for f in built/*.ckan
+do
+	mono --debug ckan.exe install -c $f
+done
