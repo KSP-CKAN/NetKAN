0b4e265120c996ebe2ebf2e4478a102e814c3ccf
Leon Wright <techman83@gmail.com>
2015-07-21 21:03:16 +0800

    symlinks are relative without full path

--- build.sh
+++ build.sh
@@ -114,7 +114,7 @@ create_dummy_ksp () {
     mono ckan.exe repo remove default
     
     # Link to the downloads cache.
-    ln -s downloads_cache dummy_ksp/CKAN/downloads
+    ln -s ../../downloads_cache/ dummy_ksp/CKAN/downloads
 }
 
 # ------------------------------------------------
