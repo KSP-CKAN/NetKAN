7e41d3b1be74adf70f852bdb5c6e70825e1f758b
Leon Wright <techman83@gmail.com>
2016-04-07 14:39:38 +0800

    Lets not validate metadata.t either

--- build.sh
+++ build.sh
@@ -206,7 +206,7 @@ echo "If you get an error below you should look for syntax errors in the metadat
 
 for f in $COMMIT_CHANGES
 do
-    if [ "$f" == "build.sh" ]
+    if [[ "$f" =~ build.sh|metadata.t ]]
     then
         echo "Lets try not to validate our build script with jsonlint"
         continue
